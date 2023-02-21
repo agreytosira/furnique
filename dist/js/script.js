1035a747a5a74e77f74b4820b3e0164151dc04b9
Agrey Tosira <agreytosira@gmail.com>
2023-02-21 09:44:42 +0700

    Add Animation on Scroll at Navbar and Update Offer Text Alignment

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
-navbarToggle = document.querySelector("#navbar-toggle");
-navbarMenu = document.querySelectorAll(".navbar-menu");
+const navbarToggle = document.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelectorAll(".navbar-menu");
 
+// Toggle Mobile Navbar
 navbarToggle.addEventListener("click", () => {
     navbarMenu.forEach((element) => {
         if (element.classList.contains("hidden")) {
@@ -11,4 +12,19 @@ navbarToggle.addEventListener("click", () => {
             element.classList.remove("flex");
         }
     });
+});
+
+const navbar = document.querySelector("#navbar");
+const jumbotron = document.querySelector("#jumbotron");
+
+// Navbar on scroll effect
+window.addEventListener("scroll", function () {
+    navbar.classList.toggle("lg:bg-body", window.scrollY > 0);
+    if (window.scrollY > 0) {
+        navbar.classList.add("lg:bg-white", "lg:py-2", "lg:shadow-md");
+        navbar.classList.remove("lg:bg-body", "lg:py-8");
+    } else {
+        navbar.classList.add("lg:bg-body", "lg:py-8");
+        navbar.classList.remove("lg:bg-white", "lg:py-2", "lg:shadow-md");
+    }
 });
